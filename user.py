cf24e235e0b8c969928b981339391c3a9a4ada65
Melvin Omega <melvinomega151@gmail.com>
2021-09-04 18:58:46 +0300

    add user list and initialised the user name and password

--- user.py
+++ user.py
@@ -1,4 +1,17 @@
 class User:
     '''
-    Generates user insan
+    Generates user insances
     '''
+
+    user_list = []
+
+    def __init__(self, user_name, password):
+        self.user_name = user_name
+        self.password = password
+
+    def save_user(self):
+        '''
+        
+        '''
+
+
